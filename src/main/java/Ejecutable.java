2983e330e05aca7aea0f4bbb33752c449930f1d6
damanzano <damh24@gmail.com>
2016-11-01 11:26:26 -0500

    Base code

---
+++ src/main/java/Ejecutable.java
@@ -0,0 +1,73 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import processing.core.PApplet;
+
+public class Ejecutable extends PApplet {
+	FirebaseDatabase database;
+	ArrayList<Bolita> bolitas;
+	
+	public static void main(String[] args) {
+		PApplet.main("Ejecutable");
+	}
+
+	public void settings() {
+		size(500, 500);
+
+	}
+
+	public void setup() {
+		// Configurar la conexión
+		try {
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setServiceAccount(new FileInputStream("src/main/resources/intro-redes-e7cc8877301b.json"))
+					.setDatabaseUrl("https://intro-redes.firebaseio.com/").build();
+			FirebaseApp.initializeApp(options);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Inicializar la referencia a la bd y las colecciones
+		database = FirebaseDatabase.getInstance();
+		bolitas = new ArrayList<Bolita>();
+		
+		// Escuchar que pasa en la bd
+		DatabaseReference ref = database.getReference("bolitas");
+		
+		ref.addValueEventListener(new ValueEventListener() {
+		    
+		    public void onDataChange(DataSnapshot dataSnapshot) {
+		        Object post = dataSnapshot.getValue();
+		        System.out.println(post);
+		        //Bolita b = dataSnapshot.getValue(Bolita.class);
+		        //bolitas.add(b);
+		    }
+		    
+		    public void onCancelled(DatabaseError databaseError) {
+		        System.out.println("The read failed: " + databaseError.getCode());
+		    }
+		});
+
+	}
+
+	public void draw() {
+		for (Iterator iterator = bolitas.iterator(); iterator.hasNext();) {
+			Bolita bolita = (Bolita) iterator.next();
+			ellipse(bolita.getX(), bolita.getY(), 50, 50);
+		}
+		
+	}
+
+}
