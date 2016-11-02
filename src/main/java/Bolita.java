83e63af48f80c5c9eb1b7e54255c5b199a358a15
damanzano <damh24@gmail.com>
2016-11-02 11:15:02 -0500

    Interacción de firebase con objetos java. Creación de nuevos elementos e inserción de los mismos en la bd

--- src/main/java/Bolita.java
+++ src/main/java/Bolita.java
@@ -21,4 +21,9 @@ public class Bolita {
 		this.y = y;
 	}
 
+	@Override
+	public String toString() {
+		return "Bolita [x=" + x + ", y=" + y + "]";
+	}
+
 }
--- src/main/java/Ejecutable.java
+++ src/main/java/Ejecutable.java
@@ -1,7 +1,9 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -14,9 +16,9 @@ import com.google.firebase.database.ValueEventListener;
 import processing.core.PApplet;
 
 public class Ejecutable extends PApplet {
-	FirebaseDatabase database;
-	ArrayList<Bolita> bolitas;
-	
+	private FirebaseDatabase database;
+	private List<Bolita> bolitas;
+
 	public static void main(String[] args) {
 		PApplet.main("Ejecutable");
 	}
@@ -33,40 +35,96 @@ public class Ejecutable extends PApplet {
 					.setServiceAccount(new FileInputStream("intro-redes-f721d3241013.json"))
 					.setDatabaseUrl("https://intro-redes.firebaseio.com/").build();
 			FirebaseApp.initializeApp(options);
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// Inicializar la referencia a la bd y las colecciones
 		database = FirebaseDatabase.getInstance();
-		bolitas = new ArrayList<Bolita>();
+		bolitas = Collections.synchronizedList(new ArrayList<Bolita>());
 		
-		// Escuchar que pasa en la bd
-		DatabaseReference ref = database.getReference("bolitas");
-		
-		ref.addValueEventListener(new ValueEventListener() {
-		    
-		    public void onDataChange(DataSnapshot dataSnapshot) {
-		        Object post = dataSnapshot.getValue();
-		        System.out.println(post);
-		        Bolita b = dataSnapshot.getValue(Bolita.class);
-		        bolitas.add(b);
-		    }
-		    
-		    public void onCancelled(DatabaseError databaseError) {
-		        System.out.println("The read failed: " + databaseError.getCode());
-		    }
-		});
+		// Consultar las bolitas iniciales
+		consultarBolitas();
 
 	}
 
 	public void draw() {
-		for (Iterator iterator = bolitas.iterator(); iterator.hasNext();) {
-			Bolita bolita = (Bolita) iterator.next();
-			ellipse(bolita.getX(), bolita.getY(), 50, 50);
+		synchronized(bolitas){
+			for (Iterator iterator = bolitas.iterator(); iterator.hasNext();) {
+				Bolita bolita = (Bolita) iterator.next();
+				ellipse(bolita.getX(), bolita.getY(), 50, 50);
+			}
 		}
+		
+
+	}
+	
+	public void mousePressed(){
+		crearBolita(mouseX,mouseY);
+	}
+
+	/**
+	 * Escuchar que pasa en la bd
+	 */
+	public void consultarBolitas() {
+		// Obtener la referencia en la que estamos interesados
+		DatabaseReference ref = database.getReference("bolitas");
+		
+		// Agregar un listener  a dicha referencia
+		ref.addValueEventListener(new ValueEventListener() {
+			
+			// Establacer que se hace cuando dicha referencia cambia
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				Object value = dataSnapshot.getValue();
+				System.out.println(value);
+
+				System.out.println("There are " + dataSnapshot.getChildrenCount() + " bolitas");
+
+				for (DataSnapshot bolitaSnapshot : dataSnapshot.getChildren()) {
+					Bolita b = bolitaSnapshot.getValue(Bolita.class);
+					System.out.println(bolitaSnapshot.getKey() + " " + b.toString());
+					bolitas.add(b);
+				}
+
+			}
+			
+			// Establecer que hacer si se cancela la operación
+			public void onCancelled(DatabaseError databaseError) {
+				System.out.println("The read failed: " + databaseError.getCode());
+			}
+		});
+	}
+	
+	/**
+	 * Crear una nueva bolita en el lienzo
+	 * @param x
+	 * @param y
+	 */
+	public void crearBolita(int x, int y){
+		// Construir el objeto
+		Bolita b = new Bolita();
+		b.setX(x);
+		b.setY(y);
+		
+		// Obtener la referencia donde vamos a guardar
+		DatabaseReference ref = database.getReference("bolitas");
+		
+		// Crear una nueva clave
+		DatabaseReference bolitaId = ref.push();
+		
+		bolitaId.setValue(b, new DatabaseReference.CompletionListener() {
+			
+			public void onComplete(DatabaseError error, DatabaseReference ref) {
+				// TODO Auto-generated method stub
+				if (error != null) {
+					System.out.println("No se pudieron guardar los datos " + error.getMessage());
+				} else {
+					System.out.println("Datos guardados satisfactoriamente.");
+				}
+			}
+		});
 		
 	}
 
